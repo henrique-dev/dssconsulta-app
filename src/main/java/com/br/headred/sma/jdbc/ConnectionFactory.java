2ccc375b1b0208c677b651b60441563575e4581c
henrique <henrique@henrique>
2019-06-14 03:00:03 -0300

    update

--- src/main/java/com/br/headred/sma/jdbc/ConnectionFactory.java
+++ src/main/java/com/br/headred/sma/jdbc/ConnectionFactory.java
@@ -19,7 +19,7 @@ public class ConnectionFactory {
         try {
             //String url = "jdbc:mysql://192.168.2.148:3306/smc?useTimezone=true&serverTimezone=UTC";
             String url = "jdbc:mysql://localhost:3306/smc?useTimezone=true&serverTimezone=UTC";
-            String user = "admin";
+            String user = "root";
             String password = "root";
             Class.forName("com.mysql.jdbc.Driver");
             return DriverManager.getConnection(url, user, password);
